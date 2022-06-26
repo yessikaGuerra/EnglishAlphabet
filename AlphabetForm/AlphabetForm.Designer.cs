9a14642f4aa23f409cbeac527ed3c93151475741
Yessika Guerra <nynguerra@hotmail.com>
2022-06-26 12:38:06 -0500

    Added backend logic and modified gui

--- AlphabetForm/AlphabetForm.Designer.cs
+++ AlphabetForm/AlphabetForm.Designer.cs
@@ -29,54 +29,69 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.TopLabel = new System.Windows.Forms.Label();
+            this.MiddleLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.ResultsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(20, 231);
+            this.textBox1.Location = new System.Drawing.Point(20, 179);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(756, 39);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // label3
+            // TopLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(146, 84);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(485, 32);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "This app displays all of the English alphabet.";
+            this.TopLabel.AutoSize = true;
+            this.TopLabel.Location = new System.Drawing.Point(20, 50);
+            this.TopLabel.Name = "TopLabel";
+            this.TopLabel.Size = new System.Drawing.Size(485, 32);
+            this.TopLabel.TabIndex = 0;
+            this.TopLabel.Text = "This app displays all of the English alphabet.";
             // 
-            // label4
+            // MiddleLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(110, 141);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(558, 32);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "To exclude letters enter them in the textbox below:";
+            this.MiddleLabel.AutoSize = true;
+            this.MiddleLabel.Location = new System.Drawing.Point(20, 97);
+            this.MiddleLabel.Name = "MiddleLabel";
+            this.MiddleLabel.Size = new System.Drawing.Size(558, 32);
+            this.MiddleLabel.TabIndex = 1;
+            this.MiddleLabel.Text = "To exclude letters enter them in the textbox below:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(322, 329);
+            this.button1.Location = new System.Drawing.Point(20, 260);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 46);
             this.button1.TabIndex = 3;
             this.button1.Text = "Display";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultsLabel
+            // 
+            this.ResultsLabel.AutoSize = true;
+            this.ResultsLabel.Location = new System.Drawing.Point(20, 340);
+            this.ResultsLabel.Name = "ResultsLabel";
+            this.ResultsLabel.Size = new System.Drawing.Size(604, 64);
+            this.ResultsLabel.TabIndex = 4;
+            this.ResultsLabel.Text = "Too many characters entered, maximum allowed is 256.\nPlease delete some character" +
+    "s.";
+            this.ResultsLabel.Visible = false;
             // 
             // AlphabetForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultsLabel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MiddleLabel);
+            this.Controls.Add(this.TopLabel);
             this.Name = "AlphabetForm";
             this.Text = "Alphabet Form";
             this.ResumeLayout(false);
@@ -86,8 +101,9 @@
 
         #endregion
         private TextBox textBox1;
-        private Label label3;
-        private Label label4;
+        private Label TopLabel;
+        private Label MiddleLabel;
         private Button button1;
+        private Label ResultsLabel;
     }
 }
--- AlphabetForm/AlphabetForm.cs
+++ AlphabetForm/AlphabetForm.cs
@@ -1,10 +1,75 @@
+using System.Text;
+
 namespace AlphabetForm
 {
     public partial class AlphabetForm : Form
     {
+        private const string TooManyCharMsg = "Too many characters entered, maximum allowed is 256." +
+            "\nPlease delete some characters.";
+
+        private const string EnglishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         public AlphabetForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 256)
+            {
+                ResultsLabel.Text = TooManyCharMsg;
+                ResultsLabel.Visible = true;
+            }
+            else
+            {
+                ResultsLabel.Text = DisplayDesiredAlphabetLetters(textBox1.Text);
+                ResultsLabel.Visible = true;
+            }
+        }
+
+        private string DisplayDesiredAlphabetLetters(string text)
+        {
+            char[] charsToExclude = text.ToUpper().ToCharArray();
+            char[] alphabetChars = EnglishAlphabet.ToCharArray();
+            var resultStrBuild = new StringBuilder("Letters: ");
+            int lettersCount = 0;
+
+            if (charsToExclude.Length == 0)
+            {
+                resultStrBuild.Append(EnglishAlphabet +
+                    $"\nTotal Letters displayed: {EnglishAlphabet.Length}");
+
+                return resultStrBuild.ToString();
+            }
+
+            foreach (char aC in alphabetChars)
+            {              
+                bool match = false;
+                
+                foreach (char c in charsToExclude)
+                {
+                    if (c == aC)
+                    {
+                        match = true;
+                        break;
+                    }
+                }
+
+                if (!match)
+                {
+                    resultStrBuild.Append(aC);
+                    lettersCount++;
+                }
+            }
+
+            resultStrBuild.AppendLine($"\nTotal Letters displayed: {lettersCount}");
+
+            return resultStrBuild.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ResultsLabel.Visible = false;
         }
     }
 }
